6e514101f121f6780229cc3b449b38442f4c05e5
Richard <richard.kingjunior1@gmail.com>
2021-03-24 19:42:08 -0300

    web socket service

--- src/app/GlobalServices/web-socket.service.ts
+++ src/app/GlobalServices/web-socket.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { io, Socket } from "socket.io-client";
+
+/******************/
+//Serviço responsavel por realizar toda a comunicação através de websockets
 
 @Injectable({
   providedIn: 'root'
 })
 export class WebSocketService {
 
-  constructor() { }
+  socket: Socket;
+
+  constructor() {
+    //inicia conexão com o web socket
+    this.socket = io();
+  }
+
+  //função para ouvir dado recebido
+  listen(eventName: string) {
+    console.log('listen');
+    return new Observable((subscriber) => {
+      this.socket.on(eventName, (data) =>{
+        subscriber.next(data);
+      })
+    })
+  }
+
+  //função para enviar dado
+  emit(eventName: string, data: any){
+    console.log(this.emit);
+    this.socket.emit(eventName,data);
+  }
 }
