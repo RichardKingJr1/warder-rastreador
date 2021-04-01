017f1ccf6c09e7c55785463dd65a5318823f9bb7
Richard <richard.kingjunior1@gmail.com>
2021-04-01 20:49:36 -0300

    iniciar transmissao

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { WebSocketService } from 'src/app/GlobalServices/web-socket.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private webSocketService:WebSocketService) { }
+
+  public rg: String | null = null;
+
+  public portariaLat:number = -22.904407;
+  public portariaLon:number = -47.001585;
+  public limiteDistanciaPortaria:number = 0.0003;
 
   ngOnInit(): void {
+  }
+
+  /******** Iniciar rastreamento **********/
+  rastrear(rg: String): void{
+    console.log('rastrear');
+    if (!navigator.geolocation) {
+      //console.log('location is not supported');
+      window.alert('Rastreio nao suportado');
+    }else{
+
+      if(rg){
+        //versão sem controle de distancia portaria
+        let dataObj = {
+          rg: rg,
+          lat: this.portariaLat,
+          lon: this.portariaLon
+        };
+
+        this.rg = rg;
+
+        this.webSocketService.emit('requestServer', dataObj);
+      };
+       
+    };
   }
 
 }
