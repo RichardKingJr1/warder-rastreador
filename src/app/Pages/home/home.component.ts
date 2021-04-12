360917246ce66af472e98e8680496154548e8677
Richard <richard.kingjunior1@gmail.com>
2021-04-12 19:34:36 -0300

    conexao e reconexao websocket

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -21,16 +21,42 @@ export class HomeComponent implements OnInit {
   public ctrl_view_espera: Boolean = true;
 
   ngOnInit(): void {
+    //ouve caso ocorra uma conexao ou reconexão e se ouver testa se o usuario ja estava logado
+    this.conexao();
+
+    //caso ja estivesse logado move para o painel de vc esta sendo rastreado, caso contrario leva para pagina inicial
+    this.testeReconexao();
+  }
+
+
+  /***** Conexão ao websocket e teste para checar que o usuario estava anteriormente conectado***** */
+  conexao(){
+    this.webSocketService.listen('connect').subscribe((data) => {
+      this.webSocketService.emit('alreadyConnected', this.rg);
+    });
+  }
+
+  /****** Ajusta o aplicativo para o caso de reconexao *****/
+  testeReconexao(){
+    this.webSocketService.listen('alreadyConnected').subscribe((data) => {
+      
+      console.log(data);
+      if(data == true){
+        this.ctrl_view = false;
+        this.ctrl_view_espera = false;
+      }else{
+        this.ctrl_view = true;
+        this.ctrl_view_espera = true;
+      }
+    });
   }
 
   /******** Iniciar rastreamento **********/
-  rastrear(rg: String): void{
+  solicitarRastreio(rg: String): void{
     console.log('rastrear');
     if (!navigator.geolocation) {
-      //console.log('location is not supported');
       window.alert('Rastreio nao suportado');
     }else{
-
       if(rg){
         //versão sem controle de distancia portaria
         let dataObj = {
@@ -45,11 +71,11 @@ export class HomeComponent implements OnInit {
         this.ctrl_view = !this.ctrl_view;
         this.ctrl_view_espera = true;
 
-      };
-       
+      };   
     };
-  }
+  };
 
+  /******* Controle do  view *******/
   viewCtrl(view:Boolean, view_espera:Boolean){
     if(this.ctrl_view == view || this.ctrl_view_espera == view_espera){
       return true;
@@ -58,5 +84,6 @@ export class HomeComponent implements OnInit {
     };
   };
 
-  
+
+
 }
